8d11408c9a04111aca8f6e5ca1d731925f59385d
riversun.org@gmail.com <riversun.org@gmail.com>
2023-06-02 10:27:17 +0900

    update example

--- examples/example.py
+++ examples/example.py
@@ -4,7 +4,8 @@ from loadtime import LoadTime
 
 model_path = "togethercomputer/RedPajama-INCITE-Chat-3B-v1"
 
-tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = LoadTime(name=model_path,
                  hf=True,
                  fn=lambda: AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16))()
+
+tokenizer = AutoTokenizer.from_pretrained(model_path) # important: load tokenizer after loading model

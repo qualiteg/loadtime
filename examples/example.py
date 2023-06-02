d8ffaabc129290fe56038184911d9f2a1454fa8b
riversun.org@gmail.com <riversun.org@gmail.com>
2023-06-02 10:59:17 +0900

    update example

--- examples/example.py
+++ examples/example.py
@@ -8,4 +8,5 @@ model = LoadTime(name=model_path,
                  hf=True,
                  fn=lambda: AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16))()
 
+
 tokenizer = AutoTokenizer.from_pretrained(model_path) # important: load tokenizer after loading model
